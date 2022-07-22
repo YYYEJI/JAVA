b1353566128829a06f1b5b73aa74b490d13a6a9b
YYYEJI <22100396@handong.ac.kr>
2022-07-22 02:10:10 -0700

    Assignment 1

---
+++ CH01/HelloWorld.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class HelloWorld {
+	public static void main(String [] agrs) {
+		System.out.println("Hello World\n");
+
+	}
+}
---
+++ CH01/YejiSong.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class YejiSong {
+	public static void main(String [] args) {
+		System.out.println("Name : YejiSong");                         // 송예
+		System.out.println("University : Handong Global University");  // 한동대학
+		System.out.println("Student Number 22100396");                 // 22100396
+	}
+}
